30ceb5f0d6ce393fda63615c0293adecf800fa9f
Mohamed Shakin <shakincc02@gmail.com>
2025-05-04 17:39:11 +0400

    Updated the slider

--- Frontend/Javascript/teamslider.js
+++ Frontend/Javascript/teamslider.js
@@ -1,13 +1,22 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
+const swiper = new Swiper('.swiper', {
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  speed: 2000,
   loop: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 });
+
+
+// Get custom buttons
+const leftArrow = document.querySelector('.arrow-left');
+const rightArrow = document.querySelector('.arrow-right');
+
+
+// Add click events to control swiper
+leftArrow.addEventListener('click', () => swiper.slidePrev());
+rightArrow.addEventListener('click', () => swiper.slideNext());
