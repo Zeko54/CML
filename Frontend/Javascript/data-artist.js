1b3f470314fd20f2a3816421ba9bb20cd16f16cc
Zoraiz Mudassar <zoraiztufail5@gmail.com>
2025-05-04 17:29:28 +0400

    Artist Pages Html
    Added Pages of all the Artists

--- Frontend/Javascript/data-artist.js
+++ Frontend/Javascript/data-artist.js
@@ -27,7 +27,203 @@ const bandData = {
     ],
   },
   "def-ph": {
-    gallery: ["gallery/def1.jpg", "gallery/def1.jpg"],
+    gallery: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR59Z01ub6p0FvGdPLtDOCHvZeruWf7DBfSeQ&s",
+      "https://wallpaper-house.com/data/out/6/wallpaper2you_82332.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSCIcG3ksnLKmolmk7j5eYnq_SDoWIPimevg&s",
+    ],
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+    ],
+  },
+  "sweet-soul": {
+    gallery: [
+      "https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2018/02/lake-kawaguchi-with-boat-800x500.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrmWtvuOKN3JjRWUBMVKSF_q1NIJapFDoAWQ&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSCIcG3ksnLKmolmk7j5eYnq_SDoWIPimevg&s",
+    ],
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+    ],
+  },
+  "D&A": {
+    gallery: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR59Z01ub6p0FvGdPLtDOCHvZeruWf7DBfSeQ&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSCIcG3ksnLKmolmk7j5eYnq_SDoWIPimevg&s",
+      "https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2018/02/lake-kawaguchi-with-boat-800x500.jpg",
+    ],
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+    ],
+  },
+  "SKA&Mash": {
+    gallery: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1T0UO2tf_S9FKfeo3tRGbVdOvD7e6dgNLwg&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP4UuFTzA8z4Vyt8LfoNU31PzqGmj8Ng0hLg&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1f5GMW8uuHvEY4ieDxhii-p_OXJDouPIJ-w&s",
+    ],
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+    ],
+  },
+  "Shamiskha-violin": {
+    gallery: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSCIcG3ksnLKmolmk7j5eYnq_SDoWIPimevg&s",
+      "https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2018/02/lake-kawaguchi-with-boat-800x500.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1T0UO2tf_S9FKfeo3tRGbVdOvD7e6dgNLwg&s",
+    ],
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+    ],
+  },
+  "Vlada-Pianist": {
+    gallery: [
+      "https://ggfx-arabianestates2.s3.eu-west-2.amazonaws.com/x.prod/800x500/azizi_venice_p1a_Gt_xl_7acb511526____stb0912.webp",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSCIcG3ksnLKmolmk7j5eYnq_SDoWIPimevg&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR59Z01ub6p0FvGdPLtDOCHvZeruWf7DBfSeQ&s",
+    ],
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+    ],
+  },
+  "Dj-aya": {
+    gallery: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL4fr5ldqQ1Ky2R_lKbWs4ncAkbZ5-zKQz4A&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSCIcG3ksnLKmolmk7j5eYnq_SDoWIPimevg&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR59Z01ub6p0FvGdPLtDOCHvZeruWf7DBfSeQ&s",
+    ],
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+    ],
+  },
+  "Dj-Freecs": {
+    gallery: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh3ua2imm5HPz26_traMlBq0noKby3spSl7Q&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSCIcG3ksnLKmolmk7j5eYnq_SDoWIPimevg&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR59Z01ub6p0FvGdPLtDOCHvZeruWf7DBfSeQ&s",
+    ],
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+      {
+        src: "https://www.youtube.com/embed/def123",
+        title: "Summer Concert Highlights",
+        description: "A collection of our best moments from the summer tour.",
+      },
+    ],
+  },
+  "G-swag": {
+    gallery: [
+      "https://the-aiff.com/media/uploads/2025/04/IMG_0896-800x500.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSCIcG3ksnLKmolmk7j5eYnq_SDoWIPimevg&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR59Z01ub6p0FvGdPLtDOCHvZeruWf7DBfSeQ&s",
+    ],
     videos: [
       {
         src: "https://www.youtube.com/embed/def123",
