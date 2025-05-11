7c6841ab7eb89b610423816422fd5bf28a4bf874
Zoraiz Mudassar <zoraiztufail5@gmail.com>
2025-05-11 20:25:04 +0400

    Artist Page
    Fixing the linking issue on the vercel website

--- Frontend/Javascript/artistslider.js
+++ Frontend/Javascript/artistslider.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const bgImages = [
-    "/Frontend/Images/ArtistPage/bg-1.jpg",
-    "/Frontend/Images/ArtistPage/bg-2.jpg",
-    "/Frontend/Images/ArtistPage/bg-3.jpg",
-    "/Frontend/Images/ArtistPage/bg-4.jpg",
+    "/Images/ArtistPage/bg-1.jpg",
+    "/Images/ArtistPage/bg-2.jpg",
+    "/Images/ArtistPage/bg-3.jpg",
+    "//Images/ArtistPage/bg-4.jpg",
   ];
 
   bgImages.forEach((img) => {
@@ -17,14 +17,14 @@ const performers = {
       name: "D & A<br>ACOUSTIC",
       description:
         "D & A Acoustic Duo is the perfect harmony of two souls, blending their unique styles into a sound that’s intimate, raw, and deeply moving. With every strum and every note, they weave genres together, turning familiar melodies into something uniquely theirs. <br /><br />More than just performers, they are storytellers—creating music that feels personal, timeless, and effortlessly connected.",
-      image: "/Frontend/Images/ArtistPage/D&A-Acoustic-Duo.webp",
+      image: "/Images/ArtistPage/D&A-Acoustic-Duo.webp",
       link: "/Pages/artists/D&A.html",
     },
     {
       name: "SKA & MASH <br> DUO",
       description:
         "DEF Band PH is the sound of fearless creativity—where dynamic rhythms, powerful vocals, and bold melodies collide. Their music isn’t just played; it’s unleashed, breaking boundaries and pulling listeners into something fresh and electrifying. <br /><br />More than a band, they are a movement—an invitation to feel, to move, and to get lost in the sound.",
-      image: "/Frontend/Images/ArtistPage/SKA&Mash-Duo.webp",
+      image: "/Images/ArtistPage/SKA&Mash-Duo.webp",
       link: "/Pages/artists/SKA&Mash.html",
     },
   ],
@@ -33,21 +33,21 @@ const performers = {
       name: "SWEET SOUL <br> BAND",
       description:
         "Sweet Soul Band is the heartbeat of timeless grooves, where smooth harmonies, infectious rhythms, and pure emotion come to life. With every note, they channel the spirit of soul, funk, and R&B, creating an experience that moves both the heart and the feet. <br /><br />More than a performance, their music is a feeling—one that lingers long after the last song fades.",
-      image: "/Frontend/Images/ArtistPage/Sweet-Soul.webp",
+      image: "/Images/ArtistPage/Sweet-Soul.webp",
       link: "/Pages/artists/sweet-soul.html",
     },
     {
       name: "ABC PH BAND",
       description:
         "ABC Band PH channels raw passion into every note, blending rock, pop, and soul into music that speaks beyond words. Their sound isn’t just heard—it’s felt, carrying the highs and lows of their journey in every melody, every beat. <br /><br />More than a band, they are emotion in motion, turning life’s stories into songs that resonate with the soul.",
-      image: "/Frontend/Images/ArtistPage/ABC-Ph.webp",
+      image: "/Images/ArtistPage/ABC-Ph.webp",
       link: "/Pages/artists/abc-ph.html",
     },
     {
       name: "DEF PH BAND",
       description:
         "DEF Band PH is the sound of fearless creativity—where dynamic rhythms, powerful vocals, and bold melodies collide. Their music isn’t just played; it’s unleashed, breaking boundaries and pulling listeners into something fresh and electrifying. <br /><br />More than a band, they are a movement—an invitation to feel, to move, and to get lost in the sound.",
-      image: "/Frontend/Images/ArtistPage/Def-Ph.webp",
+      image: "/Images/ArtistPage/Def-Ph.webp",
       link: "/Pages/artists/def-ph.html",
     },
   ],
@@ -56,14 +56,14 @@ const performers = {
       name: "SHAMIKSHA <br> THE VIOLINIST",
       description:
         "Her violin doesn’t just play notes—it tells stories, evokes emotions, and leaves audiences breathless. Whether it’s a classical masterpiece or a modern fusion, Shamiksha brings a unique energy and passion to every performance. <br /><br />Her style is more than technique; it’s an experience. Each note is a journey, every melody a memory waiting to be made.",
-      image: "/Frontend/Images/ArtistPage/Shamiksha.webp",
+      image: "/Images/ArtistPage/Shamiksha.webp",
       link: "/Pages/artists/Shamiskha-violin.html",
     },
     {
       name: "VLADA <br> SOLO PIANIST",
       description:
         "Trained in the rigorous Soviet classical tradition, she refined her artistry in Kyiv before taking her talent across the world, performing at some of the most prestigious hotels. From Turkey to the Middle East, each performance is more than music; it is a reflection of her passion, discipline, and love for the instrument. <br /><br />With every chord, she tells a story—one of dedication, elegance, and the timeless power of the piano.",
-      image: "/Frontend/Images/ArtistPage/Vlada.webp",
+      image: "/Images/ArtistPage/Vlada.webp",
       link: "/Pages/artists/Vlada-Pianist.html",
     },
   ],
@@ -72,14 +72,14 @@ const performers = {
       name: "DJ AYA",
       description:
         "A DJ, a performer, a storyteller, and a musical sorceress who transports audiences into fantastical realms with her electrifying sets. Known for blending high-energy beats with an immersive cosplay experience, DJ Aya stands at the intersection of music, gaming, anime, and pop culture. <br /><br />With every performance, she transforms into iconic characters, turning the DJ booth into a stage where sound and spectacle collide.",
-      image: "/Frontend/Images/ArtistPage/Dj-Aya.webp",
+      image: "/Images/ArtistPage/Dj-Aya.webp",
       link: "/Pages/artists/Dj-aya.html",
     },
     {
       name: "DJ FREECS",
       description:
         "He’s the pulse of the party, turning every event into an electrifying experience. With seamless genre-blending, live remixing, and creative mashups, he transforms tracks into something fresh and unexpected. <br /><br />Feeding off the crowd’s energy, he doesn’t just play music—he crafts a dynamic, immersive journey that keeps the dancefloor alive and unforgettable.",
-      image: "/Frontend/Images/ArtistPage/Dj-Freecs.webp",
+      image: "/Images/ArtistPage/Dj-Freecs.webp",
       link: "/Pages/artists/Dj-Freecs.html",
     },
   ],
@@ -88,7 +88,7 @@ const performers = {
       name: "G-SWAG DANCERS",
       description:
         "The G-Swag Dancers are the embodiment of movement, rhythm, and modern dance culture. Blending hip-hop, krumping, popping, and the latest dance trends, they bring an electrifying energy to every performance. <br /><br />With innovative choreography and a passion for self-expression, they don’t just follow the beat—they set it, redefining what it means to move in style.",
-      image: "/Frontend/Images/ArtistPage/G-Swag-Dancers.webp",
+      image: "/Images/ArtistPage/G-Swag-Dancers.webp",
       link: "/Pages/artists/G-swag.html",
     },
   ],
