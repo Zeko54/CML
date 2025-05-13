ef531f55901341863b4b0f5b0ff734f91a514cd2
Zoraiz Mudassar <zoraiztufail5@gmail.com>
2025-05-13 13:14:35 +0400

    Products Page
    Added all the products images and videos

--- Frontend/Javascript/products.js
+++ Frontend/Javascript/products.js
@@ -1,33 +1,142 @@
 const productData = {
   promoDeals: [
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJP_rbQohrXaCvxNl0cHkg7ZCK989HI9-zdQ&s",
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-UAE/370294774_855542819533582_5820480254832809225_n.jpg",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQov2BNKOXe22kDqKH-afurTMKlKDuwnMS4Q&s",
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-UAE/387337784_639978881683918_5132328481575490673_n.jpg",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJP_rbQohrXaCvxNl0cHkg7ZCK989HI9-zdQ&s",
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-UAE/387531072_339392678532684_1365176116918899349_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-UAE/387623532_663749455862094_7777685406910885572_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-UAE/387638096_695310809310772_8050674401685323124_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-DUBAI/491144256_1216448067148441_298161241210625466_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-DUBAI/491577285_1216448093815105_1907406936870148845_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-DUBAI/491606775_1216447933815121_5077657476778293204_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-DUBAI/491696818_1216448030481778_7911433778515893352_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-DUBAI/491697125_1216448063815108_8927117320992875034_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-DUBAI/492006964_1216448087148439_8935847003012929566_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-DUBAI/492045496_1216448023815112_8743892374696838332_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-DUBAI/492099632_1216446897148558_8613229394078459050_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/EXCLUSIVE-FOR-DUBAI/492423095_1216446770481904_3165758467055340933_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SPECIAL-PROMO-FOR-UAE/491262665_1215807237212524_8977663665183323835_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SPECIAL-PROMO-FOR-UAE/491397500_1215807170545864_5774168366207251107_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SPECIAL-PROMO-FOR-UAE/491800514_1215806900545891_3085651332774843670_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SPECIAL-PROMO-FOR-UAE/491826122_1215806887212559_3597742270597070724_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SPECIAL-PROMO-FOR-UAE/491888944_1215807257212522_9182856341756633827_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SPECIAL-PROMO-FOR-UAE/491996651_1215806890545892_3640179789695339522_n (1).jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/TWIN-PROMO/370089890_699570435458508_7334223369143579369_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/TWIN-PROMO/386442870_910640927220533_6699259632560070773_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/TWIN-PROMO/393227636_558325129811197_1920029854550924427_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/TWIN-PROMO/398536538_1765154513928550_2229527779440248541_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/TWIN-PROMO/399919273_1874957879586030_3174646330057797098_n.jpg",
     },
   ],
   ourProducts: [
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQov2BNKOXe22kDqKH-afurTMKlKDuwnMS4Q&s",
+      img: "/Frontend/Images/ProductsPage/PEELING-GEL/400254657_24968303506102450_6159531278455034048_n.jpg",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJP_rbQohrXaCvxNl0cHkg7ZCK989HI9-zdQ&s",
+      img: "/Frontend/Images/ProductsPage/PEELING-GEL/400496433_24968304436102357_6853952110513794744_n.jpg",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQov2BNKOXe22kDqKH-afurTMKlKDuwnMS4Q&s",
+      img: "/Frontend/Images/ProductsPage/PEELING-GEL/400570241_24968305102768957_7675427430488804572_n.jpg",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJP_rbQohrXaCvxNl0cHkg7ZCK989HI9-zdQ&s",
+      img: "/Frontend/Images/ProductsPage/PEELING-GEL/401636687_24968303349435799_4894211612578831335_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SERUM/401090997_24983303434602457_2563214751596489285_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SERUM/401530915_24983307977935336_2196515329396421224_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SERUM/401660580_24983302847935849_3064665003424626030_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SERUM/401717934_24983307981268669_6158765409277484512_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SLEEPING-PACK/400250954_24968327096100091_8148756549964467491_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SLEEPING-PACK/400285679_24968326839433450_4820374544069555759_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SLEEPING-PACK/400334109_24968327999433334_4562189736669759420_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SLEEPING-PACK/400537429_24968326766100124_2023783948726810363_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SUNBLOCK/401606257_24980662021533265_2382462133717408912_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/SUNBLOCK/401841855_24980662084866592_8435112135827877303_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/TONE-UP/400474767_24968232192776248_1625680150035128270_n.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/TONE-UP/goodsImage.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/TONE-UP/img_product_03_07_01.jpg",
+    },
+    {
+      img: "/Frontend/Images/ProductsPage/TONE-UP/img_product_03_07_02.jpg",
     },
   ],
   productVideos: [
-    { src: "https://youtu.be/jn5UNG1FPk8?si=FpdEkNfi-71o9X9x" },
-    { src: "https://youtu.be/zKAiTPjtLpo?si=hdn-BZcz-MCTXOJc" },
-    { src: "https://youtu.be/jn5UNG1FPk8?si=FpdEkNfi-71o9X9x" },
+    { src: "https://www.youtube.com/embed/epgcwt9F__g?si=4Iu6CFv2L__SkcLj" },
+    { src: "https://www.youtube.com/embed/aWpqncFljpI?si=rilnFzQjx0F8krMK" },
+    { src: "https://www.youtube.com/embed/hmRLmTxnmTE?si=93Jm0Q-Z3ulRTED7" },
+    { src: "https://www.youtube.com/embed/1w37CqfIIZE?si=4Bp-XXGrHoPnFDGy" },
   ],
 };
 
