35a27fe427fdcd3a5dfe6f83f246ff21e842dbe0
Zoraiz Mudassar <zoraiztufail5@gmail.com>
2025-05-23 21:05:29 +0400

    Update
    Added new videos to individual artist pages

--- Frontend/Javascript/data-artist.js
+++ Frontend/Javascript/data-artist.js
@@ -10,19 +10,19 @@ const bandData = {
     ],
     videos: [
       {
-        src: "https://www.youtube.com/embed/dQw4w9WgXcQ?si=7ZVacYN6HMM0pC0M",
+        src: "https://www.youtube.com/embed/zKAiTPjtLpo?si=4X6NjI8JmcACHNSh",
         title: "Corporate Christmas Party",
         description:
           "It was a pleasure to play live music for the 250 employees of Prime Projects @ Grand Plaza Movenpick DMC last Dec.21,2023",
       },
       {
-        src: "https://www.youtube.com/embed/dh3Hb004RVE?si=JnD-tLBmGf-xYxjq",
+        src: "https://www.youtube.com/embed/jn5UNG1FPk8?si=K-bLAXAR4KzRoszc",
         title: "OASIS CANTINA BAR & BISTRO",
         description:
           "We are regular every Saturday night from 10:30pm to 2:30am @ Radisson Red Hotel Dubai Silicon Oasis. 3 sets of non-stop live band music..",
       },
       {
-        src: "https://www.youtube.com/embed/-2RAq5o5pwc?si=Mw95Ym9uW_FzYmjh",
+        src: "https://www.youtube.com/embed/r0nPrxlDp18?si=x5Wy68HJWam1USD-",
         title: "Alyana's 16th Birthday Party",
         description:
           "Taking part of Alyana's birthday @Jumeirah Creekside Hotel with the whole families & friends last September 2023.",
@@ -37,19 +37,22 @@ const bandData = {
     ],
     videos: [
       {
-        src: "https://www.youtube.com/embed/def123",
-        title: "Summer Concert Highlights",
-        description: "A collection of our best moments from the summer tour.",
+        src: "https://www.youtube.com/embed/x75kEjDjOZ0?si=rL7oqo3eKS5xYnOT",
+        title: "Mogao Bar and Restaurant",
+        description:
+          "Gig @ Mogao Bar and Restaurant Radisson Red Dubai Silicon Oasis.",
       },
       {
-        src: "https://www.youtube.com/embed/def123",
-        title: "Summer Concert Highlights",
-        description: "A collection of our best moments from the summer tour.",
+        src: "https://www.youtube.com/embed/PT0nTx0iKAg?si=a_kfRZ26RII6IMrq",
+        title: "Mogao Bar and Restaurant",
+        description:
+          "Gig @ Mogao Bar and Restaurant Radisson Red Dubai Silicon Oasis.",
       },
       {
-        src: "https://www.youtube.com/embed/def123",
-        title: "Summer Concert Highlights",
-        description: "A collection of our best moments from the summer tour.",
+        src: "https://www.youtube.com/embed/YZ8mE7NU9yM?si=JDucm8CiUVRnZOhW",
+        title: "Mogao Bar and Restaurant",
+        description:
+          "Gig @ Mogao Bar and Restaurant Radisson Red Dubai Silicon Oasis.",
       },
     ],
   },
@@ -61,19 +64,22 @@ const bandData = {
     ],
     videos: [
       {
-        src: "https://www.youtube.com/embed/def123",
-        title: "Summer Concert Highlights",
-        description: "A collection of our best moments from the summer tour.",
+        src: "https://www.youtube.com/embed/MAM_cXMql_E?si=I35_5SyHNfM9Tk9u",
+        title: "Girls Just wanna Have Fun",
+        description:
+          "Performing live @ Rotary Club of Batangas (Dinner for a Cause) last April 13 2024.",
       },
       {
-        src: "https://www.youtube.com/embed/def123",
-        title: "Summer Concert Highlights",
-        description: "A collection of our best moments from the summer tour.",
+        src: "https://www.youtube.com/embed/AG5OaJTmcQs?si=UaDfQtG-798QuHHa",
+        title: "OPM Medley",
+        description:
+          "Performing live @ Rotary Club of Batangas (Dinner for a Cause) last April 13 2024.",
       },
       {
-        src: "https://www.youtube.com/embed/def123",
-        title: "Summer Concert Highlights",
-        description: "A collection of our best moments from the summer tour.",
+        src: "https://www.youtube.com/embed/t8losQM8O-M?si=jv37ZxfpWaqZbK_0",
+        title: "Tattooed on my Mind (Bossa Nova)",
+        description:
+          "Performing live @ Rotary Club of Batangas (Dinner for a Cause) last April 13 2024.",
       },
     ],
   },
